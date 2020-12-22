2348a71bd5593d2ca534adde56744f12a92d90be
Kamel Ghalgaoui <kamelghalgaoui7@gmail.com>
2020-12-23 00:06:34 +0100

    Add Autotools Files

--- src/example_thread.c
+++ src/example_thread.c
@@ -6,13 +6,15 @@
  * Posix Thread: Example 01
  ****************************************************/
 #define _GNU_SOURCE
+#define THREADS_NUMBER 10
+
 #include <sched.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
-
-#define THREADS_NUMBER 10
+#include <sched.h>
 
 struct thread_fact_params {
 	int thread_num;
@@ -44,14 +46,19 @@ void *factorial_routine(void *arg)
 
 int main(void)
 {
+	time_t start = time(NULL);
+
+	cpu_set_t cpuset;
+	pthread_attr_t attr;
+
+	//~ Table of virtual cpu
+	long cpu_t[8] = {0, 1, 2, 4, 5, 6, 7};
 	pthread_t *thread = calloc(THREADS_NUMBER, sizeof(pthread_t));
 	if (thread == NULL) {
 		fprintf(stderr, "ERROR:Problem allocation of thread table\n");
 		exit(EXIT_FAILURE);
 	}
-	//~ cpu_set_t *set = NULL;
-	int cpu_set = sysconf(_SC_NPROCESSORS_CONF);
-	fprintf(stdout, "INFO: The num of cpu: %d\n", cpu_set);
+
 	long numbers[THREADS_NUMBER] = {10, 23, 8, 5, 9, 7, 3, 12, 18, 52};
 	void *ret = NULL;
 	struct thread_fact_params *thread_params =
@@ -59,25 +66,50 @@ int main(void)
 
 	fprintf(stdout, "INFO: Launching Theards\n");
 	for (int counter = 1; counter <= THREADS_NUMBER; counter ++) {
-		int number = 0;
+		//~ int number = 0;
 		thread_params->thread_num = counter;
 		thread_params->number = numbers[counter - 1];
 
-		fprintf(stdout, "INFO: Thread %d\n", counter);
-		if (pthread_create(&(thread[counter - 1]), NULL, factorial_routine,
-			(void *) thread_params) != 0) {
-			fprintf(stderr, "ERROR starting thread %d\n", counter);
+		int count = 0;
+		while (count < 8) {
+			/* Init with attr by default */
+			pthread_attr_init(&attr);
+			//~ Preapare the cpu
+			CPU_ZERO(&(cpuset));
+			CPU_SET(cpu_t[count], &(cpuset));
+			//~ Fix affinity
+			pthread_attr_setaffinity_np(&attr,
+										sizeof(cpu_set_t),
+										&cpuset);
+			if (pthread_create(&(thread[counter - 1]), &attr,
+				factorial_routine,
+				(void *) thread_params) == 0) {
+				fprintf(stdout, "thread %d on cpu %ld created\n",
+						counter, cpu_t[count]);
+				break;
+			}
+			if (count == 7)
+				count = 0;
+			count ++;
 		}
+		//~ if (pthread_create(&(thread[counter - 1]), NULL, factorial_routine,
+			//~ (void *) thread_params) != 0) {
+			//~ fprintf(stderr, "ERROR starting thread %d\n", counter);
+		//~ }
 	}
-	fprintf(stdout, "INFO: All threads are launched\n");
+
 	//~ Join threads
 	for (int counter = 0; counter < THREADS_NUMBER; counter ++) {
 		pthread_join(thread[counter], &ret);
 		fprintf(stdout, "INFO: Result: %ld! = %llu\n", numbers[counter],
 				(unsigned long long)ret);
 	}
+
 	fprintf(stdout, "INFO: All threads done\n");
 	free(thread);
 	free(thread_params);
+	time_t end = time(NULL);
+	unsigned int time_taken = end - start;
+	fprintf(stdout, "Time program took %u seconds to execute \n", time_taken);
 	exit(EXIT_SUCCESS);
 }
